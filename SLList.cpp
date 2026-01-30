271d6356434f6d834bec5f77d56e0cc7ecceb21c
Benjamin Sedmont <bsedmont4995@floridapoly.edu>
2026-01-30 14:05:07 -0500

    Add files via upload

---
+++ SLList.cpp
@@ -0,0 +1,55 @@
+#include "SLList.h"
+#include <iostream>
+
+template<typename T>
+SLList<T>::SLList() {
+    head = nullptr;
+    tail = nullptr;
+    listSize = 0;
+}
+
+template<typename T>
+unsigned SLList<T>::size() const {
+
+}
+
+template<typename T>
+bool SLList<T>::empty() const {
+
+}
+
+template<typename T>
+void SLList<T>::push_front(const T& value) {
+    
+
+
+}
+
+template<typename T>
+void SLList<T>::print() const {
+   
+
+
+
+}
+
+template<typename T>
+void SLList<T>::push_back(const T& value) {
+    
+
+
+
+}
+
+template<typename T>
+void SLList<T>::pop_front() {
+
+ 
+  
+}
+
+template<typename T>
+void SLList<T>::pop_back() {
+
+  
+}
